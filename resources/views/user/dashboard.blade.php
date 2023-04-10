c2cb37b67c74facfacc7a4ad329b9a645d03dd53
CharlyCee <charlznjoagwu@gmail.com>
2023-04-10 22:54:35 +0100

    new olympic code

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -7,18 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('style-dashboard.css')}}">
+    <link rel="stylesheet" href="style-dashboard.css">
 </head>
 
 <body>
-    <div class="container">
+    <div class="container-fliud">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Dashboard</div>
 
                     <div class="card-body">
-                        Welcome, John Doe! You are logged in as a member of the broadcast site.
+                        Welcome, Olympic 2023! You are logged in as a member of the broadcast site.
                     </div>
                 </div>
 
@@ -27,13 +27,11 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-6">
-                                <img src="https://via.placeholder.com/150" alt="Channel Thumbnail"
-                                    class="img-thumbnail mb-2">
+                                <img src="images/back1.png" alt="Channel Thumbnail" class="img-thumbnail mb-2">
                                 <p>Channel 1</p>
                             </div>
                             <div class="col-md-6">
-                                <img src="https://via.placeholder.com/150" alt="Channel Thumbnail"
-                                    class="img-thumbnail mb-2">
+                                <img src="images/backg2.png" alt="Channel Thumbnail" class="img-thumbnail mb-2">
                                 <p>Channel 2</p>
                             </div>
                         </div>
@@ -45,13 +43,11 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-6">
-                                <img src="https://via.placeholder.com/300x150" alt="Broadcast Thumbnail"
-                                    class="img-thumbnail mb-2">
+                                <img src="images/back 3.png" alt="Broadcast Thumbnail" class="img-thumbnail mb-2">
                                 <p>Latest Broadcast 1</p>
                             </div>
                             <div class="col-md-6">
-                                <img src="https://via.placeholder.com/300x150" alt="Broadcast Thumbnail"
-                                    class="img-thumbnail mb-2">
+                                <img src="images/logoolympic.png" alt="Broadcast Thumbnail" class="img-thumbnail mb-2">
                                 <p>Latest Broadcast 2</p>
                             </div>
                         </div>
